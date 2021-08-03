846fae13fb7c1334ce72dd7330269abf5148c6af
sesaretah <hs.shafiei@gmail.com>
2021-08-04 01:10:22 +0430

    1

--- node_modules/react-datepicker2/src/index.dev.js
+++
@@ -1,7 +0,0 @@
-import './styles/style.scss';
-import momentJalaali from 'moment-jalaali';
-import DatePicker from './components/DatePicker';
-
-momentJalaali.loadPersian({ dialect: 'persian-modern' });
-export { Calendar } from './components/Calendar';
-export default DatePicker;
